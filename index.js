ca455cf1a6190aa0504b091a23adf8da82a63c0e
Keyhoh <32242766+Keyhoh@users.noreply.github.com>
2020-07-12 21:51:07 +0900

    update: fuzzy検索テスト

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const appendContent = content => {
     getRoot().appendChild(li);
 }
 const show = json => json.forEach(d => appendContent(d.fields.content_name[0]));
-const BASE_URL = 'https://198182tj3g.execute-api.ap-northeast-1.amazonaws.com/edbaseapi';
+const BASE_URL = 'https://198182tj3g.execute-api.ap-northeast-1.amazonaws.com/v1/search';
 const search = q => fetch(`${BASE_URL}?q=${q}`, {mode: 'cors'})
     .then(res => res.json())
     .then(show);
